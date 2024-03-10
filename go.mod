deb75256f547b28e9dc63616cca14e8c05e125bd
Sigitas Dagilis <sigis@seluxit.com>
2024-03-10 20:54:37 +0100

    mock implementation

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.18.2
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 )
 
 require (
--- internal/device/device.go
+++ internal/device/device.go
@@ -1,22 +1,73 @@
 package device
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/tarm/serial"
 )
+
+func lightUp(port *serial.Port, led, color string) {
+
+	dataToSend := fmt.Sprintf("%s_%s", led, color)
+	bytesToSend := []byte(dataToSend)
+
+	_, err := port.Write(bytesToSend)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("Has send " + dataToSend)
+}
 
 func Run() error {
 
 	log.Info("Device is running")
 
-	for true {
-		// Do something
-		fmt.Println("Device is running")
+	portName := "/dev/ttyACM2"
+
+	config := &serial.Config{
+		Name:        portName,
+		Baud:        9600, // Set the baud rate according to your device's specifications.
+		ReadTimeout: time.Second,
+	}
+
+	port, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer port.Close()
+
+	// 0 LED - cpp check,
+	lightUp(port, "0", "blue")
+
+	time.Sleep(300 * time.Millisecond)
+
+	lightUp(port, "1", "blue")
+
+	time.Sleep(300 * time.Millisecond)
+
+	lightUp(port, "2", "blue")
+
+	time.Sleep(300 * time.Millisecond)
+
+	lightUp(port, "3", "blue")
+	/*
+	 *    go func(failed bool) {
+	 *        blink(port, 30, "2", failed)
+	 *    }(false)
+	 *
+	 *    go func(failed bool) {
+	 *        blink(port, 30, "3", failed)
+	 *    }(true)
+	 */
+
+	// main loop
+	for {
 		time.Sleep(1 * time.Second)
 	}
 
-	return errors.New("Device is not running")
+	//return errors.New("device is not running")
 }
